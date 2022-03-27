a6b0c585a96c68291a854a9dc96cd156dede3799
alaa5090 <alaaabdraboo297@gmail.com>
2022-03-27 09:21:25 +0200

    add memoryUtilization

--- src/system.cpp
+++ src/system.cpp
@@ -32,7 +32,7 @@ for(auto i =pids.begin(); i<pids.end();i++){
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
 std::string System::OperatingSystem() { return string(); }
