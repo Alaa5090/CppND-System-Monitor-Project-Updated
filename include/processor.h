8f2b1c3585e7e1f10b55ed7b95485d0285653e90
alaa5090 <alaaabdraboo297@gmail.com>
2022-03-28 16:43:03 +0200

    add constructor

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,24 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
-
+#include <array>
 class Processor {
  public:
+
+  Processor();  
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
+ long getTotalJiffies();
+  // TODO: Declare any necessary private members
  private:
+ void CalculateDeltaTime(std::array<long,3>&,std::array<long,3>&);
+ virtual void GetLinuxParserForJiffies(std::array<long,3>&);
+ float CalculateUtilization();
+ void UpdatePrevJiffies();
+ std::array<long,3> current_cpu_jiffies_;
+ std::array<long,3> prev_cpu_jiffies_;
+ std::array<float,3> delta_jiffies_;
 };
+
 
 #endif
