ce1214e6a714fbfbaab122d8cb51104f4a4a10cb
alaa5090 <alaaabdraboo297@gmail.com>
2022-03-28 15:48:22 +0200

    add  CalculateDeltaTime impl

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,18 @@
 #include "processor.h"
-
+#include "linux_parser.h"
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+Processor::Processor()
+{
+    GetLinuxParserForJiffies(prev_cpu_jiffies_);
+}
+
+float Processor::Utilization() { 
+    return 0.0; }
+
+    void Processor::CalculateDeltaTime(std::array<long,3>&first_cpu_jiffies,std::array<long,3>&second_cpu_jiffies)
+ {
+     for(int i=0;i<3;i++)
+     {
+        delta_jiffies_[i]=first_cpu_jiffies[i]-second_cpu_jiffies[i];
+     }
+ }
